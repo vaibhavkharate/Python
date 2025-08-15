552d4d59f361bbc46c1af180d56e387a70d9e7e0
Vaibhav Kharate <vaibhav.s.kharate@gmail.com>
2025-08-15 20:07:07 +0530

    some basics about python

---
+++ 01_basics/greet.py
@@ -0,0 +1,18 @@
+from hello import greet # This imports the greet function from the hello module.
+# This is a simple Python script that imports a function from another module and uses it. 
+
+greet("Bob")
+
+
+
+# """ some Theory about python
+
+# - Python Virtual Machine (PVM): The PVM is the runtime engine of Python that executes the bytecode. It is responsible for interpreting the bytecode and executing it on the host machine.
+# - Python Interpreter: The Python interpreter is the program that reads and executes Python code. It can be run in interactive mode or script mode.
+# - code loop to iterate byte code: The Python interpreter reads the bytecode and executes it line by line. It uses a loop to iterate through the bytecode instructions and execute them one by one.
+# - run time engine: The runtime engine is responsible for executing the Python code. It manages memory allocation, garbage collection, and other runtime tasks.
+
+
+# byte code is not machine Code
+# python specific interpretation
+# cpython, jython, pypy, ironpython
---
+++ 01_basics/hello.py
@@ -0,0 +1,11 @@
+# "_" is been prefred more than the "camelCasing" in Python.
+
+print("Hello, World!")
+# This is a simple Python script that prints "Hello, World!" to the console.
+
+
+# This is an method in Python.
+def greet(name):
+    print("Hello..!", name)
+
+greet("Alice")
