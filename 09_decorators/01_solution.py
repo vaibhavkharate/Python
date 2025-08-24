f825087a9016da4335f67fffe339dd7e868b7f30
Vaibhav Kharate <vaibhav.s.kharate@gmail.com>
2025-08-24 17:51:36 +0530

    Decorators in python

---
+++ 09_decorators/01_solution.py
@@ -0,0 +1,15 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time: {end_time - start_time} seconds")
+        return result
+    return wrapper
+
+@timer
+def slow_function():
+    time.sleep(2)
+    print("Finished slow function")
